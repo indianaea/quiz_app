1ea1c52c9c0b71e207928e28d1200bad6d1654d6
indianaea <126889057+indianaea@users.noreply.github.com>
2023-04-10 21:47:22 +0000

    stíla texta

--- lib/answer.dart
+++ lib/answer.dart
@@ -3,16 +3,20 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   final VoidCallback selectHandler;
 
-  Answer();
+  Answer(this.selectHandler);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: ElevatedButton(
-        color: Colors.blue,
-        textColor: Colors.white,
-        child: Text('Answer 1'),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.blue),
+        ),
+        child: Text(
+          'Answer 1',
+          style: TextStyle(color: Colors.black),
+        ),
         onPressed: selectHandler, //onPressed takes a function
       ),
     );
